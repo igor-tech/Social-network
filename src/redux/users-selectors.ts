ffa98512287df1d199576a6aacb2489cc4f5a0dd
Igor Shargin <igor20513@gmail.com>
2023-03-09 20:36:28 +0500

    create test superSelector

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,6 +1,12 @@
 import {AppStateType} from './redux-store';
+import {createSelector} from 'reselect';
+
 
 export const getUsersSelector = (state: AppStateType) =>  state.usersPage.users
+export const getUsersSuper = createSelector(getUsersSelector, (users: any) => {
+    return users.filter((u: any) => true)
+})
+
 export const getPageSizeSelector = (state: AppStateType) =>  state.usersPage.pageSize
 export const getTotalUsersCountSelector = (state: AppStateType) =>  state.usersPage.totalUsersCount
 export const getTotalCurrentPageSelector = (state: AppStateType) =>  state.usersPage.currentPage

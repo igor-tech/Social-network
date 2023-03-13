d0789aa564b71e0662ab5fdc6923b753fe2d68ea
Igor Shargin <igor20513@gmail.com>
2023-03-13 21:27:46 +0500

    fix: calcSlicedPages logic

--- src/utils/calcSlicedPages.ts
+++ src/utils/calcSlicedPages.ts
@@ -10,7 +10,7 @@ export function calcSlicedPages({pageSize, currentPage, totalUsersCount}: curren
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
-    let curPF = ((currentPage - 5) < 0) ? 0 : currentPage - 5;
-    let curPL = currentPage + 5;
+    let curPF = ((currentPage - 4) < 0) ? 0 : currentPage - 4;
+    let curPL = currentPage > 4 ? currentPage + 4 : 8;
     return pages.slice(curPF, curPL)
 }
